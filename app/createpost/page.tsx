688fd80759bd43e569c68e1a26e421c880158a5c
viv007084 <vivekguptatekari@gamil.com>
2023-07-25 18:31:36 +0530

    gjggjh

--- app/createpost/page.tsx
+++ app/createpost/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import  useRouter  from "next/navigation";
+import  {useRouter}  from "next/navigation";
 import { useState, useEffect } from "react";
 
 const page = () => {
